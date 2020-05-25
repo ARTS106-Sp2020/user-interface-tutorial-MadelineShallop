2322047bfaab41913d779a54a549818113b97eab
MadelineShallop <61506957+MadelineShallop@users.noreply.github.com>
2020-05-25 10:20:39 -0400

    Animating the buttons

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+
+    public void OpenSettings()
+    {
+
+        startButton.SetBool("isHidden", true);
+
+        settingsButton.SetBool("isHidden", true);
+    }
    public void StartGame()
     {
 
