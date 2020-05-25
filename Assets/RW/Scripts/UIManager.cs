6e8187e161884090145d81132f74b35bc91f405a
MadelineShallop <61506957+MadelineShallop@users.noreply.github.com>
2020-05-25 10:47:54 -0400

    Settings Dialog Box

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -7,6 +7,7 @@ public class UIManager : MonoBehaviour
 {
     public Animator startButton;
     public Animator settingsButton;
+    public Animator dialog;
 
     public void OpenSettings()
     {
@@ -14,6 +15,17 @@ public class UIManager : MonoBehaviour
         startButton.SetBool("isHidden", true);
 
         settingsButton.SetBool("isHidden", true);
+
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+
+        startButton.SetBool("isHidden", false);
+
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
    public void StartGame()
     {
